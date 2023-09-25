aec3250f326d625a5e55917002ab8df613f07475
Gabriel França <dev.gabrielfranca@gmail.com>
2023-09-25 17:47:16 -0300

    fix: change about text and section title margin

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -5,7 +5,7 @@ export const Section = (props: ComponentProps<'section'>) => (
 )
 
 export const SectionTitle = (props: { children: ReactNode }) => (
-  <div className="sticky top-0 z-20 -mx-6 bg-neutral-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only">
+  <div className="sticky -top-px z-20 -mx-6 bg-neutral-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only">
     <h2
       className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only"
       {...props}
