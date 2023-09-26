60d68ca1880ddca70bbe3d9d99394c668a41dde7
Gabriel França <dev.gabrielfranca@gmail.com>
2023-09-26 12:02:11 -0300

    feat: adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 import { Inter } from 'next/font/google'
 import { ReactNode } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Layout(props: { children: ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body
         className={`${inter.className} bg-neutral-900 leading-relaxed text-neutral-400 antialiased selection:bg-neutral-200 selection:text-neutral-900`}
-        {...props}
-      />
+      >
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
