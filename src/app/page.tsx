c531cf11fb2843935e49432fbb83262868a1b988
Gabriel França <dev.gabrielfranca@gmail.com>
2023-10-02 14:11:22 -0300

    feat: updating contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -180,7 +180,7 @@ export default function Home() {
                 target="_blank"
               >
                 <span className="border-b border-transparent pb-px transition group-hover:border-neutral-300 motion-reduce:transition-none">
-                  View Full Resume
+                  View Resume
                 </span>
                 <FiArrowRight className="ml-1 inline-block h-4 w-4 transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none" />
               </a>
